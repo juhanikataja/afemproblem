2040282bd4a644839108b79aff6f6aff1ca7dd9c
Juhani Kataja <juhani.m.kataja@aalto.fi>
2022-12-22 17:35:40 +0200

    hacky assembly of RT elements

--- runme.jl
+++ runme.jl
@@ -7,11 +7,9 @@ using Ferrite
 using FerriteGmsh
 using Logging, LoggingExtras
 
-debug_logger = ConsoleLogger(stderr, Logging.Debug)
-global_logger(debug_logger);
 
 element_dim = 2
-ambient_dim = 2
+ambient_dim = 3
 
 gmsh.initialize()
 gmsh.open("box.msh")
@@ -32,7 +30,6 @@ struct RaviartThomas{dim, shape, order} <: VectorInterpolation{dim, shape,order}
 #struct Lagrange{refdim, ambientdim, shape, order} <: Interpolation{refdim, ambientdim, shape, order} end
 
 import .Ferrite.AbstractRefShape
-Ferrite.debug_mode()
 Ferrite.getnbasefunctions(::RaviartThomas{2,RefTetrahedron,1}) = 3
 #Ferrite.getlowerdim(::RaviartThomas{2, RefTetrahedron, 1}) = RaviartThomas{1, RefCube, 1}
 Ferrite.nvertexdofs(::RaviartThomas{2, RefTetrahedron, 1}) = 0
@@ -82,8 +79,9 @@ struct CellRTValues{dim,T<:Real,refshape<:AbstractRefShape,M} <: CellValues{dim,
     detJdV::Vector{T} # Volume element in reference element frame ($\sqrt{\det g_{ij}}$, where $g_ij = J_i ⋅ J_j$ with $J_i$ being the i:th row of reference element mapping jacobian)
     M::Matrix{T} # Geometric interpolation function values
     dMdξ::Matrix{Vec{dim,T}} # Derivatives of interpolation function wrt reference element coordinates
-    qr::QuadratureRule{dim,refshape,T}
     J::Matrix{T} # Jacobian of reference element mapping
+    det_gij::Vector{T} # volume element
+    qr::QuadratureRule{dim,refshape,T}
     # The following fields are deliberately abstract -- they are never used in
     # performance critical code, just stored here for convenience.
     func_interp::Interpolation{dim,refshape}
@@ -102,7 +100,7 @@ function CellRTValues( ::Type{T},
 
     # Function interpolation
     n_func_basefuncs = getnbasefunctions(func_interpol)
-    N    = fill(zero(Vec{dim,T})      * T(NaN), n_func_basefuncs, n_qpoints)
+    N    = fill(zero(Vec{dim,T})      * T(NaN), n_func_basefuncs, n_qpoints) 
     dNdx = fill(zero(Tensor{2,dim,T}) * T(NaN), n_func_basefuncs, n_qpoints)
     dNdξ = fill(zero(Tensor{2,dim,T}) * T(NaN), n_func_basefuncs, n_qpoints)
 
@@ -110,6 +108,8 @@ function CellRTValues( ::Type{T},
     n_geom_basefuncs = getnbasefunctions(geom_interpol)
     M    = fill(zero(T)          * T(NaN), n_geom_basefuncs, n_qpoints)
     dMdξ = fill(zero(Vec{dim,T}) * T(NaN), n_geom_basefuncs, n_qpoints)
+    J = fill(zero(T)*T(NaN), 3, 2) # Assumes R^2 -> R^3 mapping of reference element
+    det_gij = fill(zero(T)*T(NaN), 1)
 
     for (qp, ξ) in enumerate(quad_rule.points)
         basefunc_count = 1
@@ -126,37 +126,50 @@ function CellRTValues( ::Type{T},
     detJdV = fill(T(NaN), n_qpoints)
     MM = Tensors.n_components(Tensors.get_base(eltype(dNdx)))
 
-    CellRTValues{dim,T,shape,MM}(N, dNdx, dNdξ, detJdV, M, dMdξ, quad_rule, func_interpol, geom_interpol)
+    CellRTValues{dim,T,shape,MM}(N, dNdx, dNdξ, detJdV, M, dMdξ, J, det_gij, quad_rule, func_interpol, geom_interpol)
 end
 
 function Ferrite.reinit!(cv::CellRTValues, x::AbstractVector{Vec{dim,T}}) where {dim, T}
-    @debug display(cv)
-    F = (x[3]-x[1], x[2]-x[1])  
 
+    # Jacobian of affine reference-to-global element mapping (modulo hand miscalculations)
+    cv.J[:,1] = x[3]-x[1]
+    cv.J[:,2] = x[2]-x[1]
+    cv.det_gij[1] = sqrt(cv.J[:,1]⋅cv.J[:,1] * cv.J[:,2]⋅cv.J[:,2] - cv.J[:,1]⋅cv.J[:,2]*cv.J[:,2]⋅cv.J[:,1])
+
+    cv.detJdV[:] = cv.det_gij[1] * cv.qr.weights[:]
+    
 end
 
-dh = with_logger(debug_logger) do
-    dh = DofHandler(gridi)
-    push!(dh, :u, 1, RaviartThomas{2, RefTetrahedron, 1}())
-    close!(dh)
-    dh
-end
+dh = DofHandler(gridi)
+push!(dh, :u, 1, RaviartThomas{2, RefTetrahedron, 1}())
+close!(dh)
+dh
 
-K = create_sparsity_pattern(dh)
+
 
 function loop_cells(cellvalues, 
-    K::SparseMatrixCSC, 
+    K::SparseMatrixCSC, f::Vector{Float64},
     dh::DofHandler)
     n_basefuncs = getnbasefunctions(cellvalues)
+    local_mat = fill(zero(Float64), 3, 3)
+    local_f = fill(zero(Float64), 3)
+    u = fill(zero(Float64), 3, 3)
+    A = start_assemble(K,f)
     for (cellcount, cell) in enumerate(CellIterator(dh))
         reinit!(cellvalues, cell.coords)
-        display(typeof(cellvalues))
-        dΩ = getdetJdV(cellvalues, 1)
-        δu = shape_value(cellvalues, 1, 1) 
-        #map((δu, dΩ)) do x display(x) end
-        #display(getnquadpoints(cellvalues))
-        coords=getcoordinates(cell)
-        dofs=celldofs(cell)
+        for (qp, ξ) in enumerate(cellvalues.qr.points)
+            for n in 1:getnbasefunctions(cellvalues.func_interp)
+                u[:,n] = cellvalues.J*value(cellvalues.func_interp, n, ξ) ./ cellvalues.det_gij[1]
+            end
+            for m in 1:3
+                for n in 1:3
+                    local_mat[m,n] += u[:,m]⋅u[:,n]*cellvalues.detJdV[qp]
+                end
+                local_f[m] += u[3,m] * cellvalues.detJdV[qp] # L2-interpolate constant vector field u_z
+            end
+        end
+        display(local_mat)
+        assemble!(A, celldofs(cell), local_mat, local_f)
     end
 end
 
@@ -165,4 +178,8 @@ qr = QuadratureRule{element_dim, RefTetrahedron}(2)
 geom_ip = Lagrange{element_dim, RefTetrahedron, 1}()
 
 cellvalues = CellRTValues(Float64, qr, ip, geom_ip)
-loop_cells(cellvalues, K, dh)
+
+K = create_sparsity_pattern(dh)
+f = zeros(ndofs(dh))
+
+loop_cells(cellvalues, K,f, dh)
